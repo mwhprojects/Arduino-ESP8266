ed38d12ec7bf9f6f063cdca09258b46457a6692f
asimowalk5 <asimowalk@gmail.com>
2015-01-10 14:15:11 -0500

    Arduino Code

--- ThingSpeak_DHT22_public.ino
+++ ThingSpeak_DHT22_public.ino
@@ -1,20 +1,19 @@
+// Set up DHT22 library.
 #include "DHT.h"
 #define DHTPIN A1
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-#define SSID "[MY_SSID]"
-#define PASS "[MY_PASSWORD]"
+// Set up macros for wifi and Thing Speak connection.
+#define SSID "[MY_SSID]"		// SSID
+#define PASS "[MY_PASSWORD]"	// Password
+#define TSKEY "[MY_TS_KEY]"		// Thingspeak Key
 #define IP "184.106.153.149"    // thingspeak.com
-#define TSKEY "[MY_TS_KEY]" // Thingspeak Key
-
-int brightness;
 
 void setup()
 {
-  dht.begin();
-  
-  pinMode(A0, INPUT);  // LDR
+  dht.begin();	// DHT22
+  pinMode(A0, INPUT);	// LDR
   
   Serial.begin(9600);
   Serial.println("AT");
@@ -24,20 +23,27 @@ void setup()
   }
 }
 
+int brightness;		// Variable for LDR
+
 void loop(){
-  brightness = analogRead(A0);
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
+  brightness = analogRead(A0);			// Get LDR reading
+  float t = dht.readTemperature();		// Get temperature (in C) from DHT22 sensor
+  float h = dht.readHumidity();			// Get humidity (in %) from DHT22 sensor
+  
+  // If there is an error with the DHT22 sensor readings, send obvious bad reading.
   if (isnan(h) || isnan(t)) {
-    t = -1;
-    h = -1;
+    t = -99;
+    h = -99;
     return;
   }
-  sendData(String(brightness), String(t), String(h));
-  delay(120000);
+  
+  
+  sendData(String(brightness), String(t), String(h));	// Call function to send data to Thing Speak.
+  delay(120000);	// Send data every 2 minutes.
 }
 
 void sendData(String tBrightness, String tTemp, String tHum){
+  // Set up TCP connection.
   String cmd = "AT+CIPSTART=\"TCP\",\"";
   cmd += IP;
   cmd += "\",80";
@@ -46,6 +52,8 @@ void sendData(String tBrightness, String tTemp, String tHum){
   if(Serial.find("Error")){
     return;
   }
+  
+  // Send data.
   cmd = "GET /update?key=";
   cmd += TSKEY;
   cmd += "&field1=";
